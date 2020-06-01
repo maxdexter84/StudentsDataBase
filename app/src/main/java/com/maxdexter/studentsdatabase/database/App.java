d67e249dcd72a497c329235efc863b745a9eff5b
maxdexter <maxdexter@bk.ru>
2020-06-01 15:43:57 +0300

    создал синглтон класс App наследующий Application. В этом классе будет создаваться база данных

--- app/src/main/java/com/maxdexter/studentsdatabase/database/App.java
+++ app/src/main/java/com/maxdexter/studentsdatabase/database/App.java
@@ -2,5 +2,34 @@ package com.maxdexter.studentsdatabase.database;
 
 import android.app.Application;
 
+import androidx.room.Room;
+
+// Паттерн Singleton, наследуем класс Application, создаём базу данных
+// в методе onCreate
 public class App extends Application {
+    private static App instance;
+    //База данных
+    private EducationDatabase db;
+
+    public static App getInstance() {
+        if(instance == null){
+            instance = new App();
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Сохраняем объект приложения (для Singleton’а)
+        instance = this;
+        //Строим базу данных
+        db= Room.databaseBuilder(getApplicationContext(),EducationDatabase.class,"education_database")
+                .allowMainThreadQueries()//только для примеров и тестирования
+                .build();
+    }
+    // Получаем EducationDao для составления запросов
+    public EducationDao getEducationDao(){
+        return db.getEducationDao();
+    }
 }
---
+++ app/src/main/java/com/maxdexter/studentsdatabase/ui/EducationSource.java
@@ -0,0 +1,4 @@
+package com.maxdexter.studentsdatabase.ui;
+
+public class EducationSource {
+}
